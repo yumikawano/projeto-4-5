8dd78158aa9f9c4865e6cdbf9457a2ccdfbccc2e
yumikawano <camila.domingos@al.infnet.edu.br>
2022-06-08 20:16:24 -0300

    instala firebase

---
+++ front-end/src/services/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBylsrzdNdEz2JuZ_A57PXWCKVpGH0ixss",
+  authDomain: "magia-das-velas-ababb.firebaseapp.com",
+  projectId: "magia-das-velas-ababb",
+  storageBucket: "magia-das-velas-ababb.appspot.com",
+  messagingSenderId: "50685209881",
+  appId: "1:50685209881:web:30f89c01907cf91e49a988"
+};
+
+const app = initializeApp(firebaseConfig);
