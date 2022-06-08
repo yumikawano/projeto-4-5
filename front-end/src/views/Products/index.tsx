0b50a8be47c933e32fded3f08f26323b4cd1e94f
yumikawano <camila.domingos@al.infnet.edu.br>
2022-06-08 19:17:54 -0300

    remove api

--- front-end/src/views/Products/index.tsx
+++ front-end/src/views/Products/index.tsx
@@ -1,12 +1,8 @@
-import { CardProducts } from "../../components/CardProducts";
 import { Layout } from "../../components/Layout";
 
 export function ProductsView () {
     return (
         <Layout>
-            <CardProducts>
-
-            </CardProducts>
         </Layout>
     )
 }
